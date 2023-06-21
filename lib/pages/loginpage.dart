1933b766cde1da21c438a5acbe7e78b9e6a27d0c
Pratyoosh <pratyooshanand00.17@gmail.com>
2023-06-21 17:10:01 +0530

    day 4

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,16 +7,58 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
-        style: TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_1.png"),
+          SizedBox(height: 20,
+          ),
+          Text("Welcome",
+          style: GoogleFonts.lato(
+            textStyle: TextStyle(
+              color: Colors.blue,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            )
+          ),
 
-        ),
-        ),
-      ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 30,),
+          ElevatedButton(
+            child: Text("Login"),
+            onPressed: (){
+            },
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all(Colors.white),
+              backgroundColor: MaterialStateProperty.all(Colors.indigo)
+
+            )),
+
+
+
+
+        ],
+      )
     );
   }
 }
